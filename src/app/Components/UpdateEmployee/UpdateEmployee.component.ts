d1710b36bd092f85585a7a456ebade3753af2a81
Muhammed Magdy <muhammedmagdy653@gmail.com>
2022-05-24 23:17:06 +0200

    adding forms

--- src/app/Components/UpdateEmployee/UpdateEmployee.component.ts
+++ src/app/Components/UpdateEmployee/UpdateEmployee.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../../Services/Employee.service';
+import { Employee } from '../../Models/Employee';
+import { FamilyMember } from 'src/app/Models/FamilyMember';
 
 @Component({
   selector: 'app-UpdateEmployee',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./UpdateEmployee.component.css']
 })
 export class UpdateEmployeeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router:Router,private myroute:ActivatedRoute,private service:EmployeeService) { }
+  Employee:Employee=new Employee([]);
+@ViewChild('MemberName',{read:ElementRef}) MemberName!: ElementRef;
+@ViewChild('MemberRelation',{read:ElementRef}) MemberRelation!: ElementRef;
+    ngOnInit(): void {
+      this.service.getEmployee(this.myroute.snapshot.params['id']).subscribe(
+        emp=>{this.Employee=emp;});
+    }
+  Update(){
+    console.log(this.Employee);
+    this.service.UpdateEmployee(this.Employee).subscribe(res=> this.router.navigate(['employees']));
+  }
+  AddMember(){
+    this.Employee.familyMembers.push({id:0,name:this.MemberName.nativeElement.value,relation:this.MemberRelation.nativeElement.value,employeeId:0});
+    this.MemberName.nativeElement.value='';
+    this.MemberRelation.nativeElement.value='';
+  }
+  DeleteMember(item:FamilyMember){
+    let index=this.Employee.familyMembers.findIndex(e=>e===item);
+    this.Employee.familyMembers.splice(index,1);
   }
 
 }
